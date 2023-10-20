e8dea4193f49c5a2d273ce31d2d1b28e77c78d50
clariva27 <meyclariva@gmail.com>
2023-10-20 11:18:27 +0700

    API and Bootstrap

--- app/Views/product.php
+++ app/Views/product.php
@@ -10,30 +10,32 @@
 
 </head>
 <body>
-    <h1>Data Products</h1>
-    <table class="table table-bordered table-striped" cellpadding="10">
-        <thead class="table-dark">
-            <tr>
-                <th>ID</th>
-                <th>Nama Product</th>
-                <th>Deskripsi</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data as $p): ?>
-            <tr>
-                <td><?=$p->id?></td>
-                <td><?=$p->nama_product?></td>
-                <td><?=$p->description?></td>
-                <td>
-                    <a href="<?=base_url('edit-product/' . $p->id)?>" class="btn btn-primary">Edit</a>
-                    <a href="<?=base_url('delete-product/'.$p->id)?>" class="btn btn-danger">Hapus</a>
-                </td>
-            </tr>
-            <?php endforeach?>
-        </tbody>
-    </table>
+    <div class="container">
+        <h1>Data Products</h1>
+        <table class="table table-bordered table-striped" cellpadding="10">
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Nama Product</th>
+                    <th>Deskripsi</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($data as $p): ?>
+                <tr>
+                    <td><?=$p->id?></td>
+                    <td><?=$p->nama_product?></td>
+                    <td><?=$p->description?></td>
+                    <td>
+                        <a href="<?=base_url('edit-product/' . $p->id)?>" class="btn btn-primary">Edit</a>
+                        <a href="<?=base_url('delete-product/'.$p->id)?>" class="btn btn-danger">Hapus</a>
+                    </td>
+                </tr>
+                <?php endforeach?>
+            </tbody>
+        </table>
+    </div>
 
     <!-- Add Bootstrap JS script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
